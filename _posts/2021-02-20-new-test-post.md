e443dcc235a0a4fd1158811bf92f298e1b67279c
Axel Sparkster <axelsparkster@gmail.com>
2021-02-20 00:16:26 -0600

    Initial commit

---
+++ _posts/2021-02-20-new-test-post.md
@@ -0,0 +1,6 @@
+---
+title: New Test Post
+date: 2021-02-20 00:12 -0600
+---
+
+Nothing too interesting here. Just a test post.
---
+++ _site/assets/js/lunr/lunr-store.js
@@ -0,0 +1,7 @@
+var store = [{
+        "title": "New Test Post",
+        "excerpt":"Nothing too interesting here. Just a test post.  ","categories": [],
+        "tags": [],
+        "url": "/new-test-post/",
+        "teaser": null
+      }]
